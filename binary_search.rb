437cbf28ffc048be153384ba4618935167bd00dc
chris-wong-1 <wong.christopher.s@gmail.com>
2016-08-14 12:50:51 -0700

    Add binary_search method

---
+++ binary_search.rb
@@ -0,0 +1,21 @@
+# The array given to the binary_search function should already be sorted
+# The value is the element in the array that is being searched for
+
+def binary_search(array, value)
+  low = 0
+  high = array.length - 1
+
+  while (low <= high)
+    mid = low + ( (high-low) / 2 )
+
+    if array[mid] == key
+      return mid
+    elsif array[mid] < key
+      low = mid + 1
+    else
+      high = mid + 1
+    end
+  end
+
+  return "Value not found in given array"
+end
