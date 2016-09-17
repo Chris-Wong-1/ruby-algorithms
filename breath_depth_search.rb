adf1e4b9ac52bb25d19d793612dfe4d8993a1160
chris-wong-1 <wong.christopher.s@gmail.com>
2016-09-16 23:52:35 -0700

    Create breath_depth_search.rb and add Node class

---
+++ breath_depth_search.rb
@@ -0,0 +1,21 @@
+class Node
+  attr_accessor :value, :left, :right, :name
+
+  def initialize(options={})
+    @value = options[:value]
+    @name = options[:name]
+  end
+
+  def children
+    [@left, @right].compact
+  end
+
+  def children?
+    @left && @right
+  end
+
+  def no_children?
+    !children?
+  end
+  
+end
