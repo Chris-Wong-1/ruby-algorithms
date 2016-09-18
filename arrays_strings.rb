f957e79aced74a929104628b846808c913c327bd
chris-wong-1 <wong.christopher.s@gmail.com>
2016-09-18 15:38:40 -0700

    Add unique_with_hash? method

--- arrays_strings.rb
+++ arrays_strings.rb
@@ -4,3 +4,21 @@ def unique?(str)
 end
 
 p unique?("cat")
+p unique?("mom")
+
+def unique_with_hash?(str)
+  chars_hash = Hash.new(0)
+  chars = str.split("")
+  chars.each do |char|
+    chars_hash[char] += 1
+  end
+  chars_hash.values.each do |value|
+    if value > 1
+      return false
+    end
+    return true
+  end
+end
+
+p unique_with_hash?("cat")
+p unique_with_hash?("mom")
