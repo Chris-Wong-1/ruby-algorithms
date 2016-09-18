5d73f63d71fd6f9c6b1d7ba0ee1f966c6b108bd7
chris-wong-1 <wong.christopher.s@gmail.com>
2016-09-18 15:48:19 -0700

    Add permutation method

--- arrays_strings.rb
+++ arrays_strings.rb
@@ -1,10 +1,12 @@
+# 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
 def unique?(str)
   chars = str.split("")
   chars.uniq.length == str.length
 end
 
-p unique?("cat")
-p unique?("mom")
+# p unique?("cat")
+# p unique?("mom")
 
 def unique_with_hash?(str)
   chars_hash = Hash.new(0)
@@ -20,5 +22,14 @@ def unique_with_hash?(str)
   end
 end
 
-p unique_with_hash?("cat")
-p unique_with_hash?("mom")
+# p unique_with_hash?("cat")
+# p unique_with_hash?("mom")
+
+# 1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+
+def permutation(str1, str2)
+  str1.split('').sort == str2.split('').sort
+end
+
+p permutation('cat', 'tac')
+p permutation('cat', 'dog')
