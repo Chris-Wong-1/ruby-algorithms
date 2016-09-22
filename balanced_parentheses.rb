0ae57d932c7483c5b4a4e5e0041ade232ac1301e
chris-wong-1 <wong.christopher.s@gmail.com>
2016-09-22 11:48:04 -0700

    Add valid_string? method

--- balanced_parentheses.rb
+++ balanced_parentheses.rb
@@ -1,10 +1,22 @@
 def valid_string?(str)
+  stack = []
   str.split("").each do |char|
-    char
+    if char == "("
+      stack.push(char)
+    elsif char == ")"
+      if stack.empty?
+        return false
+      else
+        stack.pop
+      end
+    end
   end
+  stack.empty?
 end
 
 p valid_string?("(hello)")
 p valid_string?("(hello) (world)")
 p valid_string?("((cat)")
 p valid_string?(")dog(")
+p valid_string?("((((()))))")
+p valid_string?("((((())))))")
