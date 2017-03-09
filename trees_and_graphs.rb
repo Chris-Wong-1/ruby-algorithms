50677d761c521b23f20a013b0b76cfd8d379b517
chris-wong-1 <wong.christopher.s@gmail.com>
2017-03-09 13:03:29 -0800

    Add example directed graph

--- trees_and_graphs.rb
+++ trees_and_graphs.rb
@@ -16,7 +16,7 @@ end
 
 class Node
   attr_accessor :value, :children
-  def initialize(value, children)
+  def initialize(value, children = [])
     @value = value
     @children = children
   end
@@ -26,3 +26,15 @@ class Node
 end
 
 # 4.1 Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+
+z = Node.new("z")
+d = Node.new("d")
+c = Node.new("c", [d])
+b = Node.new("b", [c, d])
+a = Node.new("a", [b, z])
+
+directed_graph = Graph.new([a, b, c, d, z])
+
+def direct_route(graph)
+
+end
