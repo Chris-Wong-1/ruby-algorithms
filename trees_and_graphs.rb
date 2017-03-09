7dd7a11869c87f53f74dc6f5ea4b24ecfcf160f5
chris-wong-1 <wong.christopher.s@gmail.com>
2017-03-08 18:13:07 -0800

    Add Tree, Graph and Node classes

--- trees_and_graphs.rb
+++ trees_and_graphs.rb
@@ -1,3 +1,28 @@
 # Chapter 4 Trees and Graphs
 
+class Tree
+  attr_accessor :root
+  def initialize(root = Node.new)
+    @root = root
+  end
+end
+
+class Graph
+  attr_accessor :nodes
+  def initialize(nodes)
+    @nodes = nodes
+  end
+end
+
+class Node
+  attr_accessor :value, :children
+  def initialize(value, children)
+    @value = value
+    @children = children
+  end
+  def to_s
+    value.to_s
+  end
+end
+
 # 4.1 Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
