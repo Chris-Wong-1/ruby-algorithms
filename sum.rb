6da0ecee55d6371757e42a77f79f32af3b08320c
chris-wong-1 <wong.christopher.s@gmail.com>
2017-04-06 10:58:29 -0700

    Add method for greatest sum of a subset

--- sum.rb
+++ sum.rb
@@ -13,5 +13,25 @@ def non_consec_sum(arr)
   [incl, excl].max
 end
 
-p non_consec_sum([1, 2, 3])
-p non_consec_sum([5, 5, 10, 100, 10, 5])
+# p non_consec_sum([1, 2, 3])
+# p non_consec_sum([5, 5, 10, 100, 10, 5])
+
+# Given an unsorted array of pos/neg values find the greatest sum of a continuous subset
+
+def subset_sum(arr)
+  max_so_far = 0
+  max_ending_here = 0
+
+  arr.each do |el|
+    max_ending_here = max_ending_here + el
+    if max_ending_here < 0
+      max_ending_here = 0
+    end
+    if max_so_far < max_ending_here
+      max_so_far = max_ending_here
+    end
+  end
+  max_so_far
+end
+
+p subset_sum([-1, -2, 3, 2, -1, 5, -1])
